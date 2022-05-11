ec55c1b7e9454782c8ef958c07e2033feb0a9cd3
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-11 17:17:15 +0800

    Add button to index.html to control dimensions, group grid drawing code in a function for ease of redrawing grid

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // 1. Creating 16 x 16 grid of square divs.
 const drawingGrid = document.querySelector(".drawingGrid");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    for (let j = 0; j < 16; j++) {
-        const entry = document.createElement('div');
-        entry.classList.add("gridElement");
-        entry.style.width = `${512 / 16}px`;
-        entry.style.height = `${512 / 16}px`;
-        entry.addEventListener('mouseover', (event) => {
-            entry.classList.add("hoverGridElement");
-        });
-        entry.addEventListener('transitionend', (event) => {
-            entry.classList.remove("hoverGridElement");
-        })
-        row.appendChild(entry);  
+function drawGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        for (let j = 0; j < n; j++) {
+            const entry = document.createElement('div');
+            entry.classList.add("gridElement");
+            entry.style.width = `${512 / n}px`;
+            entry.style.height = `${512 / n}px`;
+            entry.addEventListener('mouseover', (event) => {
+                entry.classList.add("hoverGridElement");
+            });
+            entry.addEventListener('transitionend', (event) => {
+                entry.classList.remove("hoverGridElement");
+            })
+            row.appendChild(entry);  
+        }
+        drawingGrid.appendChild(row);
     }
-    drawingGrid.appendChild(row);
 }
+
+drawGrid(16);
+
+// 2. Customizing the size of the
